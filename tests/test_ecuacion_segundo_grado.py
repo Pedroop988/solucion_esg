bd98b9e6c516b0452faa5f0523d95b4ba7998975
Pedroop988 <71127853@continental.edu.pe>
2025-04-30 11:38:26 -0500

    Se implementó cálculo de raíces reales

--- tests/test_ecuacion_segundo_grado.py
+++ tests/test_ecuacion_segundo_grado.py
@@ -1,5 +1,18 @@
 import unittest
+from src.logica.ecuacion_segundo_grado import EcuacionSegundoGrado
 
 
-class MyTestCase(unittest.TestCase):
-    pass
+class TestEcuacionSegundoGrado(unittest.TestCase):
+    def test_solucionESG_parametrosNumericos_raicesReales(self):
+        # Arrange
+        ecuacionSegundoGrado = EcuacionSegundoGrado()
+        ecuacionSegundoGrado.a = 3
+        ecuacionSegundoGrado.b = -5
+        ecuacionSegundoGrado.c = 1
+        RaizEsperada1 = 1.43
+        RaizEsperada2 = 0.23
+        # Do
+        RaizActual1, RaizActual2 = ecuacionSegundoGrado.solucionESG()
+        # Assert
+        self.assertAlmostEqual(RaizEsperada1, RaizActual1, 2)
+
